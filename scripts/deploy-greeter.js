cf831fe2a7c4af60f1fa50a37ea1b559c65af9a5
TudorTacal <tudor.tacal@blockfi.com>
2022-10-29 21:36:11 +0100

    feat: setup react app and hardhat node

---
+++ scripts/deploy-greeter.js
@@ -0,0 +1,14 @@
+const hre = require('hardhat');
+
+async function main() {
+  const Greeter = await hre.ethers.getContractFactory('Greeter');
+  const greeter = await Greeter.deploy('Hello World');
+  await greeter.deployed();
+
+  console.log(`contract successfully deployed to ${greeter.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
