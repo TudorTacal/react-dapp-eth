48013147ccdd82491b3eee712c8e8f053b327ea5
TudorTacal <tudor.tacal@blockfi.com>
2022-10-30 21:59:50 +0000

    feat: add fetchGreeting and requestAccount functions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
 import logo from './logo.svg';
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
 
 function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  async function requestAccount() {
+    await window.ethereum.requst({ method: 'eth_requestAccounts' });
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        const data = await contract.greet();
+        console.log('data:', data);
+      } catch (err) {
+        console.log('Error:', err);
+      }
+    }
+  }
+
+  async function setGreeting() {}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
